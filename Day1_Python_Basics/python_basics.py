ad1da8a9632c1a1faa535c9be0b65de0e84604a1
Ankush Dubey <ankushdubey2222@gmail.com>
2026-03-23 18:44:03 +0530

    Day 1 : Python basics (operators, input/output)

---
+++ Day1_Python_Basics/python_basics.py
@@ -0,0 +1,12 @@
+# Day 1: Python Basics
+
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+name = input("Enter your name: ")
+print("Hello", name)
