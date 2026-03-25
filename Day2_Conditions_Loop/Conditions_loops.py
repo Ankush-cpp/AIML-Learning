beafd87e4903b5024f4cc47184c44f50bf5932cd
Ankush Dubey <ankushdubey2222@gmail.com>
2026-03-25 23:12:42 +0530

    Added content to Day 2 files

---
+++ Day2_Conditions_Loop/Conditions_loops.py
@@ -0,0 +1,26 @@
+# Day 2: Conditions & Loops
+
+# Conditions--->
+a = 10
+b = 5
+
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a is equal to b")
+else:
+    print("b is greater than a")
+
+# Loops--->
+
+# For loop
+print("For loop:")
+for i in range(1, 6):
+    print(i)
+
+# While loop
+print("While loop:")
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
